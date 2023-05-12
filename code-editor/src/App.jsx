06260e12c6d69cd25b6b3603bb3e22ce048c361e
zilenmodi <zilen.m@simformsolutions.com>
2023-05-12 19:21:21 +0530

    Add editor

--- code-editor/src/App.jsx
+++ code-editor/src/App.jsx
@@ -3,24 +3,22 @@ import Editor from "react-simple-code-editor";
 import { highlight, languages } from "prismjs/components/prism-core";
 import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism.css"; //Example style, you can use another
-import MonacoEditor from "react-monaco-editor";
+import "prismjs/themes/prism.css";
 
 function App() {
   const [code, setCode] = React.useState(
     `const socket = io("https://server-domain.com"); \nconst socket = io("wss://server-domain.com"); \nconst socket = io("server-domain.com"); // only in the browser when the page is served over https (will not work in Node.js)`
   );
-  const options = {
-    selectOnLineNumbers: true,
-  };
   return (
-    <MonacoEditor
-      width="800"
-      height="600"
-      language="javascript"
-      theme="vs-dark"
+    <Editor
       value={code}
-      options={options}
+      onValueChange={(code) => setCode(code)}
+      highlight={(code) => highlight(code, languages.js)}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 16,
+      }}
     />
   );
 }
